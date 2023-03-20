0fffce375e4bdbae7fee3dd80aeea078c72529e2
Piotr <Piotr@DESKTOP-OGB5VIU>
2023-03-20 15:38:36 +0100

    + get intervals endpoint

--- src/main/java/com/example/deansoffice/controller/WorkerController.java
+++ src/main/java/com/example/deansoffice/controller/WorkerController.java
@@ -14,6 +14,8 @@ import com.example.deansoffice.service.WorkDateIntervalsService;
 import com.example.deansoffice.service.WorkDateService;
 import com.example.deansoffice.service.WorkerService;
 import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.deansoffice.entity.Student;
@@ -22,9 +24,11 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.ZoneId;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/workers")
@@ -48,7 +52,43 @@ public class WorkerController {
         return ResponseEntity.ok(workersDTO);
     }
 
+    @GetMapping("/{id}/workdays/{date}/intervals")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ResponseEntity<Map<String,List<String>>> getWorkDayIntervals(@PathVariable int id, @PathVariable long date) {
+        Optional<Worker> worker = workerService.getWorkerById(id);
+
+        if(worker.isPresent()) {
+
+            LocalDate convertedDate = Instant.ofEpochMilli(date)
+                    .atZone(ZoneId.systemDefault())
+                    .toLocalDate();
+
+            Optional<WorkDate> workDate = worker.get().getWorkDates().stream()
+                    .filter(d -> d.getDate().equals(convertedDate))
+                    .findFirst();
+
+            if(workDate.isPresent()) {
+                List<WorkDateIntervals> workDateIntervalsList = workDate.get().getWorkDateIntervals();
+
+                if(workDateIntervalsList != null) {
+                    List<String> intervals = workDateIntervalsList.stream().filter(w -> !w.getTaken()).map(w -> w.getStartInterval().toString() + " - " + w.getEndInterval().toString()).collect(Collectors.toList());
+                    Map<String, List<String>> intervalsJson = new HashMap<>();
+                    intervalsJson.put("intervals", intervals);
+
+                    return ResponseEntity.ok().body(intervalsJson);
+                } else {
+                    throw new IntervalNotFoundException();
+                }
+            } else {
+                throw new WorkDateNotFoundException();
+            }
+        } else {
+            throw new WorkerNotFoundException();
+        }
+    }
+
     @PostMapping("/{id}/workdays")
+    @CrossOrigin(origins = "http://localhost:3000")
     public ResponseEntity<String> newWorkDayForWorker(@PathVariable int id, @RequestBody NewWorkDayRequest request) {
         Optional<Worker> worker = workerService.getWorkerById(id);
 
@@ -62,6 +102,7 @@ public class WorkerController {
     }
 
     @PutMapping("/{id}/workdays/{date}/interval/{time}/student/{student_id}")
+    @CrossOrigin(origins = "http://localhost:3000")
     @Transactional
     public ResponseEntity<String> makeAppointment(@PathVariable int id, @PathVariable long date, @PathVariable String time, @PathVariable int student_id, @RequestBody(required = false) Map<String, String> descriptionBody) {
 
@@ -85,8 +126,6 @@ public class WorkerController {
                     .findFirst();
 
             if (workDate.isPresent()) {
-                workDate.get().getWorkDateIntervals().forEach(i -> System.out.println(i.getStartInterval()));
-
                 Optional<WorkDateIntervals> workDateInterval = workDate.get().getWorkDateIntervals().stream()
                         .filter(i -> i.getStartInterval().equals(appointmentTime))
                         .findFirst();
